25549037d012833be612a85b3bfa7127cf6851cf
Mohit <mohit.motwani20@st.niituniversity.in>
2023-02-26 15:24:29 +0530

    login and singup screens created

---
+++ lib/FrontEnd/AuthUI/sign_up.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+import 'common_auth_methods.dart';
+
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  final GlobalKey<FormState> _signUpKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+          backgroundColor: const Color.fromRGBO(34, 48, 68, 1),
+          body: Container(
+            child: ListView(
+              shrinkWrap: true,
+              children: [
+                SizedBox(height: 50.0,),
+                Center(
+                  child: Text('Sign-Up', style: TextStyle(fontSize: 28.0, color: Colors.white),),
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height/1.65,
+                  width: MediaQuery.of(context).size.width,
+                  padding: EdgeInsets.only(top: 40.0, bottom: 10.0),
+                  child: Form(
+                    child: ListView(
+                      children: [
+                        commonTextFormField(hintText: 'Email'),
+                        commonTextFormField(hintText: 'Password'),
+                        commonTextFormField(hintText: 'Conform Password'),
+                        authButton(context, 'Sign-Up'),
+                      ],
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Text('Or Continue With',
+                    style: TextStyle(color: Colors.white,fontSize: 20.0),
+                  ),
+                ),
+                socialMediaIntegrationButtons(),
+                switchAnotherAuthString(context, 'Already have an account? ', 'Log-In')
+              ],
+            ),
+          ),
+        ),
+    );
+  }
+
+
+}
